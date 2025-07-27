1a3008e230910c96662ad5bcd5cc4e2be27d95c7
Ibrar Khan <ibrarkhan431414@gmail.com>
2025-07-27 20:49:40 +0500

    splash screen added

---
+++ lib/features/view/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                  image: AssetImage('assets/images/4.png'))
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
